43abf631f5f8e3f62d36dd45c46929ea965d2f88
Darwineer <michaelaokeefejr@gmail.com>
2017-04-17 15:29:00 -0400

    Updated build.gradle repositories, changed gradle-wrapper.properties because proguard is no longer used

--- app/src/main/java/com/bourke/glimmr/common/Keys.java
+++ app/src/main/java/com/bourke/glimmr/common/Keys.java
@@ -2,6 +2,6 @@ package com.bourke.glimmr.common;
 
 public class Keys {
 
-    public static final String API_KEY = "";
-    public static final String API_SECRET = "";
+    public static final String API_KEY = "9385635dc8889f7458de50be57f84f41";
+    public static final String API_SECRET = "277e23b320d0ecd2";
 }
